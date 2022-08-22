81a6d1ab4cc324393ee82e0448f064974a29af38
baltasar <baltasarq@gmail.com>
2022-08-22 21:37:33 +0200

    Adds first steps to backtracking

--- src/GameArray1D.java
+++ src/GameArray1D.java
@@ -4,67 +4,9 @@ import java.util.Scanner;
 
 
 public class GameArray1D {
-    public static int walk(final int[] BOARD, int pos)
+    public static boolean canReachEnd(final int[] BOARD, int leap)
     {
-        while( pos < BOARD.length
-            && BOARD[ pos ] == 0 )
-        {
-            pos += 1;
-        }
 
-        if ( pos < BOARD.length
-          && pos > 0 )
-        {
-            pos -= 1;
-        }
-
-        return pos;
-    }
-
-    public static int jump(final int[] BOARD, int leap, int pos)
-    {
-        int oldPos = pos;
-        int toret = pos + leap;
-
-        while( toret < BOARD.length
-            && BOARD[ toret ] != 0 )
-        {
-            // Try to go back, if possible, and jump again
-            // in order to try to find a 0 in between the 1's.
-            if ( pos > 0
-              && BOARD[ pos - 1 ] == 0 )
-            {
-                pos -= 1;
-                toret -= 1;
-            } else {
-                break;
-            }
-        }
-
-        if ( toret < BOARD.length
-          && BOARD[ toret ] != 0 )
-        {
-            toret = oldPos;
-        }
-
-        return toret;
-    }
-
-    public static boolean canReachEnd(final int[] BOARD, int leap, int startPos)
-    {
-        int oldie = -1;
-        int i = startPos;
-
-        while( i < BOARD.length
-            && oldie != i )
-        {
-            oldie = i;
-
-            i = jump( BOARD, leap, i );
-            i = walk( BOARD, i );
-        }
-
-        return i >= BOARD.length;
     }
 
     public static boolean canWin(final int[] BOARD, int leap)
@@ -73,15 +15,7 @@ public class GameArray1D {
         int i = leap;
 
         if ( i < BOARD.length ) {
-            int startPos = walk( BOARD, 0 );
-
-            toret = false;
-            while( !toret
-                && startPos >= 0 )
-            {
-                toret = canReachEnd( BOARD, leap, startPos );
-                startPos -= 1;
-            }
+            toret = canReachEnd( BOARD, leap );
         }
 
         return toret;
